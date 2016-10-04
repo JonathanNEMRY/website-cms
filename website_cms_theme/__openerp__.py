89fa2465b42e73ebe3b3ec80837f4c0336583e08
Jonathan Nemry <jonathan.nemry@acsone.eu>
2016-10-04 16:30:12 +0100

    [FIX] reviews

--- website_cms_theme/__openerp__.py
+++ website_cms_theme/__openerp__.py
@@ -9,7 +9,7 @@
     'author': 'ACSONE SA/NV',
     'website': "http://acsone.eu",
     'category': 'Website',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'base',
